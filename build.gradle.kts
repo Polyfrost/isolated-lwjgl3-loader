52189cae0aa80d607627619572d65a5487461ecb
Deftu <me@deftu.dev>
2024-10-07 13:33:56 +0200

    Add publishing block

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,28 @@ dependencies {
     compileOnly("org.lwjgl:lwjgl:3.3.3")
     compileOnly("net.minecraft:launchwrapper:1.12")
 }
+
+publishing {
+    repositories {
+        fun MavenArtifactRepository.setupPolyfrostRepo() {
+            credentials(PasswordCredentials::class.java)
+
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+
+        maven {
+            name = "polyfrostReleases"
+            url = uri("https://repo.polyfrost.org/releases")
+            setupPolyfrostRepo()
+
+        }
+
+        maven {
+            name = "polyfrostSnapshots"
+            url = uri("https://repo.polyfrost.org/snapshots")
+            setupPolyfrostRepo()
+        }
+    }
+}
